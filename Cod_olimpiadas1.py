f97f521e23a164deb9c8f13754ae7032e7cd914d
KFFM30200 <145489523+KFFM30200@users.noreply.github.com>
2023-09-21 09:44:04 -0600

    Update Cod_olimpiadas1.py

--- Cod_olimpiadas1.py
+++ Cod_olimpiadas1.py
@@ -10,3 +10,20 @@ def area_triangulo (altura):
   altura = float(input())
 print("¿Cual es la base?")
 base = float(input(base*altura)/2)
+
+
+def  ("Multiplicación")
+
+print (a*b)
+x(a*b)
+return ("x")
+
+def ("divicion")
+print a/b
+x/(a*b)
+return ("x")
+print("Dame tu primer numero")
+a= ("Valor ingresado por el usuario")
+print(Dame el segundo numero)
+b=print("segundo numero")
+Resultado= primer numero + segundo numero 
